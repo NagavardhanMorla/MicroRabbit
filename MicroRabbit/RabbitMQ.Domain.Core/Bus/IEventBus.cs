88a1b651446f0c0c73f8118973fa9c9a6463e8c8
vardhan Morla <vardhanm576@gmail.com>
2021-07-21 09:37:52 +0530

    project architecture

---
+++ MicroRabbit/RabbitMQ.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,18 @@
+﻿using RabbitMQ.Domain.Core.Commands;
+using RabbitMQ.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+        void Publish<T>(T @event) where T : Event;
+        void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>;
+    }
+}
---
+++ MicroRabbit/RabbitMQ.Domain.Core/Events/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMQ.Domain.Core.Events
+{
+    public abstract class Event
+    {
+        public DateTime TimeStamp { get; protected set; }
+        public Event()
+        {
+            TimeStamp = DateTime.Now;
+        }
+    }
+}
